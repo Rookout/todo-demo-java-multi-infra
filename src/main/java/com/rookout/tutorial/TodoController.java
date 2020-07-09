cadb200906cf9e7a58beafc872d31b8d06ea5cd5
Mickael Alliel <mickael@rookout.com>
2020-07-09 12:32:51 +0300

    Fix lines so i dont have to change tests

--- src/main/java/com/rookout/tutorial/TodoController.java
+++ src/main/java/com/rookout/tutorial/TodoController.java
@@ -1,5 +1,4 @@
 package com.rookout.tutorial;
-
 import io.joshworks.restclient.http.HttpResponse;
 import io.joshworks.restclient.http.JsonNode;
 import io.joshworks.restclient.http.Unirest;
@@ -8,10 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.bugsnag.Bugsnag;
 import io.sentry.Sentry;
-
 import java.util.*;
 
 @RestController
